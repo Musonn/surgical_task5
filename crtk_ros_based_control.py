b66c311c53b12f94197dbaa6586d622d067bb8b7
Musonn <43937020+Musonn@users.noreply.github.com>
2022-01-03 18:36:59 -0500

    able to close the jaw now

--- crtk_ros_based_control.py
+++ crtk_ros_based_control.py
@@ -76,7 +76,7 @@ namespace = "/CRTK/"
 arm_name = "psm1"
 measured_js_name = namespace + arm_name + "/measured_js"
 measured_cp_name = namespace + arm_name + "/measured_cp"
-measured_jaw_jp_name = namespace + arm_name + '/jaw' #/servo_jp'
+servo_jaw_jp_name = namespace + arm_name + '/jaw/servo_jp'
 servo_jp_name = namespace + arm_name + "/servo_jp"
 servo_cp_name = namespace + arm_name + "/servo_cp"
 
@@ -85,17 +85,19 @@ measured_js_sub = rospy.Subscriber(
 measured_cp_sub = rospy.Subscriber(
     measured_cp_name, TransformStamped, measured_cp_cb, queue_size=1)
 measured_jaw_jp_sub = rospy.Subscriber(
-    measured_jaw_jp_name, JointState, measured_jaw_jp, queue_size=1)
+    servo_jaw_jp_name, JointState, measured_jaw_jp, queue_size=1)
 
 servo_jp_pub = rospy.Publisher(servo_jp_name, JointState, queue_size=1)
 servo_cp_pub = rospy.Publisher(servo_cp_name, TransformStamped, queue_size=1)
-servo_jaw_pub = rospy.Publisher(servo_jp_name, JointState, queue_size=1)
+servo_jaw_pub = rospy.Publisher(servo_jaw_jp_name, JointState, queue_size=1)
 
 rate = rospy.Rate(50)
 
 servo_jp_msg = JointState()
 servo_jp_msg.position = [0., 0., 1.0, 0., 0., 0.]
-#print(psm1._jaw_angle)
+
+servo_jaw_angle = JointState()
+servo_jaw_angle.position = [0.1, 0, 0, 0, 0, 0]
 
 servo_cp_msg = TransformStamped()
 servo_cp_msg.transform.translation.z = -1.0
@@ -139,10 +141,12 @@ while not rospy.is_shutdown():
         
     # ######
     # The following 3 lines move the first two joints in a sinusoidal pattern
+    # The fourth line specified the jaw angle
     elif key == 2:
         servo_jp_msg.position[0] = 0.2 * math.sin(rospy.Time.now().to_sec())
         servo_jp_msg.position[1] = 0.2 * math.cos(rospy.Time.now().to_sec())
         servo_jp_pub.publish(servo_jp_msg)
+        servo_jaw_pub.publish(servo_jaw_angle)
 
     # ######
     # The following 3 lines move the robot in cartesian space in sinusoidal fashion
--- needle_operation.py
+++ needle_operation.py
@@ -13,7 +13,7 @@ class RobotData:
     def __init__(self):
         self.measured_js = JointState()
         self.measured_cp = TransformStamped()
-
+        self.measured_jaw_jp = JointState()
 
 robData = RobotData()
 
@@ -25,6 +25,9 @@ def measured_js_cb(msg):
 def measured_cp_cb(msg):
     robData.measured_cp = msg
 
+def measured_jaw_jp(msg):
+    robData.measured_jaw_jp = msg
+
 
 rospy.init_node("sur_chal_crtk_test")
 
@@ -32,6 +35,7 @@ namespace = "/CRTK/"
 arm_name = "psm1"
 measured_js_name = namespace + arm_name + "/measured_js"
 measured_cp_name = namespace + arm_name + "/measured_cp"
+servo_jaw_name = namespace + arm_name + '/jaw/servo_jp'
 servo_jp_name = namespace + arm_name + "/servo_jp"
 servo_cp_name = namespace + arm_name + "/servo_cp"
 
@@ -42,10 +46,17 @@ measured_cp_sub = rospy.Subscriber(
 
 servo_jp_pub = rospy.Publisher(servo_jp_name, JointState, queue_size=1)
 servo_cp_pub = rospy.Publisher(servo_cp_name, TransformStamped, queue_size=1)
+servo_jaw_pub = rospy.Publisher(servo_jaw_name, JointState, queue_size=1)
 
 rate = rospy.Rate(50)
 
 '''don't change above'''
+
+# set a jaw angle
+servo_jaw_angle_closed = JointState()
+servo_jaw_angle_closed.position = [0.1, 0, 0, 0, 0, 0]
+servo_jaw_angle_open = JointState()
+servo_jaw_angle_open.position = [0.5, 0, 0, 0, 0, 0]
 
 servo_cp_msg = TransformStamped()
 servo_cp_msg.transform.translation.x = 0.27
@@ -64,7 +75,9 @@ while not rospy.is_shutdown():
     valid_key = False
     key = None
     while not valid_key:
-        key = input("Press: \n")
+        key = input("Press: \n"
+                    '1 - change x coordinate \n'
+                    '2 - close the jaw \n')
 
         try:
             key = int(key)
@@ -77,20 +90,26 @@ while not rospy.is_shutdown():
         else:
             print("Invalid Entry")
         if key == 1:
-            servo_cp_msg.transform.translation.x = input()
-            print('changed x')
+            servo_cp_msg.transform.translation.x = float(input('type the x value you want \n'))
+            print('changed x \n')
+            servo_cp_msg.transform.translation.y = float(input('type the y value you want \n'))
+            print('changed y \n')
+            servo_cp_msg.transform.translation.z = float(input('type the z value you want \n'))
+            print('changed z \n')
 
         if key == 2:
         # use attach_needle.py
+            servo_jaw_pub.publish(servo_jaw_angle_open)
             c = Client('attach_needle')
             c.connect()
             # create psm arm
-            arm1 = psm_arm.PSM(c, 'arm1')
+            # arm1 = psm_arm.PSM(c, 'arm1')
             needle = c.get_obj_handle('Needle')
             link1 = c.get_obj_handle('psm1' + '/toolyawlink')
             attach_needle(needle, link1)
             # arm1.run_grasp_logic(0.1)
-            time.sleep(3)
+            time.sleep(0.5)
+            servo_jaw_pub.publish(servo_jaw_angle_closed)
             c.clean_up()
 
         servo_cp_pub.publish(servo_cp_msg)
